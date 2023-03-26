55492ffd4dddf9822d672afdf63d5d4751b11183
ulasdemir472 <ulasdemir472@gmail.com>
2023-03-26 23:42:41 +0300

    template engine hbs eklendi ve static kullanimi

--- Express/controllers/messages.controller.js
+++ Express/controllers/messages.controller.js
@@ -1,6 +1,12 @@
+const path = require("path");
+
 function getMessages(req, res) {
   //function keyword ü hata alırsak bulmamızda yardımcı olabilmesi için tercih edilir
-  res.send("<ul><li>Hello there</li></ul>");
+  //res.send("<ul><li>Hello there</li></ul>");
+  res.sendFile(
+    path.join(__dirname, "..", "public", "images", "skimountain.jpg")
+  );
+  // dirname bu dosyanın bulunduğu dosyayı yani controllers'ı gösterir.
 }
 
 function postMessage(req, res) {
--- Express/server.js
+++ Express/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const friendsRouter = require("./routes/friends.router");
 const messagesRouter = require("./routes/messages.router");
 
 const app = express();
 const PORT = 3000;
+
+app.set("view engine", "hbs");
+app.set("viewa", path.join(__dirname, "views"));
 
 app.use((req, res, next) => {
   const start = Date.now();
@@ -13,8 +17,15 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.baseUrl}${req.url} ${end}ms`);
 });
 
+app.use("/site", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Skiing",
+    caption: "Let's Go Skiing!",
+  });
+});
 app.use("/friends", friendsRouter);
 app.use("/messages", messagesRouter);
 
