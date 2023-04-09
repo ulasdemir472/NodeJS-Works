23162124db7005930b442b388ab46558a1ce48f1
ulasdemir472 <ulasdemir472@gmail.com>
2023-04-09 20:47:38 +0300

    planets backend yazildi

---
+++ Projects/NASA-Project/server/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const planetsRouter = require("./routes/planets/planets.router");
+const cors = require("cors");
+
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(express.json());
+
+app.use(planetsRouter);
+
+module.exports = app;
